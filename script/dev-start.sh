ffe72fd11a3b7b44591743dc4dc428af77a5fe3f
Leonardo Rocha <leonardosrocha@outlook.com>
2018-10-24 10:30:25 -0300

    Fixing "dev" user creation

--- script/dev-start.sh
+++ script/dev-start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+if [ ! -z "$NGINX_UID" ]; then
+    if [ -z "$NGINX_GID" ]; then
+        NGINX_GID="$NGINX_UID"
+    fi
 
-export PUID="$NGINX_UID"
-export PGID="$NGINX_GID"
+    deluser nginx
+    addgroup -g $NGINX_GID nginx
+    adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u $NGINX_UID nginx
+
+    if [ ! -z "$DEV_UID" ]; then
+        adduser -D -S -s /bin/bash -G nginx -u $DEV_UID dev
+        su dev -c "echo \"alias mage='php -d memory_limit=4G -f /var/www/html/bin/magento'\" > /home/dev/.bashrc"
+    fi
+
+    unset PUID
+    unset PGID
+fi
 
 source /start.sh
-
-if [ ! -z "$DEV_UID" ]; then
-    adduser -D -S -s /bin/bash -G nginx -u $DEV_UID dev
-    su dev -c "echo \"alias mage='php -d memory_limit=4G -f /var/www/html/bin/magento'\" > /home/dev/.bashrc"
-fi
